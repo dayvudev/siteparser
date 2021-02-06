410a0438f934f9025c565deb188cc73faca180aa
Davide Zastawny <davide.zastawny@gmail.com>
2021-02-06 10:44:43 +0100

    Package changes: Execution Service Update

--- src/DatabaseComponent/Resource/Marker/ExecutionServiceInterface.php
+++ src/DatabaseComponent/Resource/Marker/ExecutionServiceInterface.php
@@ -1,8 +1,21 @@
 <?php declare(strict_types=1);
 namespace App\DatabaseComponent\Resource\Marker;
 
+use App\DatabaseComponent\Business\Event\EventInterface;
 use App\DatabaseComponent\Work\Service\ServiceInterface;
 
 interface ExecutionServiceInterface extends MarkerInterface, ServiceInterface
 {
+    public const EXCEPTION_MESSAGE_EMPTY_EVENT = 'Event is empty!';
+    
+    public function dispatchBeforeEvent(): void;
+    public function dispatchAfterEvent(): void;
+    /**
+     * @throws Exception
+     */
+    public function getBeforeEvent(): EventInterface;
+    /**
+     * @throws Exception
+     */
+    public function getAfterEvent(): EventInterface;
 }
--- src/DatabaseComponent/Work/Service/Execution/ConfigurationService.php
+++ src/DatabaseComponent/Work/Service/Execution/ConfigurationService.php
@@ -1,12 +1,65 @@
 <?php declare(strict_types=1);
 namespace App\DatabaseComponent\Work\Service\Execution;
 
+use App\DatabaseComponent\Business\Event\EventInterface;
 use App\DatabaseComponent\Resource\Marker\ExecutionServiceInterface;
+use App\DatabaseComponent\Work\Observer\Dispatcher\Configuration\AfterEventDispatcher;
+use App\DatabaseComponent\Work\Observer\Dispatcher\Configuration\BeforeEventDispatcher;
+use Exception;
 
 class ConfigurationService implements ExecutionServiceInterface
 {
+    private $beforeEventDispatcher;
+    private $afterEventDispatcher;
+
+    private $beforeEvent;
+    private $afterEvent;
+
+    public function __construct(
+        BeforeEventDispatcher $beforeEventDispatcher,
+        AfterEventDispatcher $afterEventDispatcher
+    ) {
+        $this->beforeEventDispatcher = $beforeEventDispatcher;
+        $this->afterEventDispatcher = $afterEventDispatcher;
+    }
+
     public function execute(): void
     {
+        $this->dispatchBeforeEvent();
+        $this->dispatchAfterEvent();
+    }
+    
+    public function dispatchBeforeEvent(): void
+    {
+        $this->beforeEvent = $this->beforeEventDispatcher->dispatch();
+    }
 
+    public function dispatchAfterEvent(): void
+    {
+        $this->afterEvent = $this->afterEventDispatcher->dispatch();
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function getBeforeEvent(): EventInterface
+    {
+        if (! $this->beforeEvent instanceof EventInterface) {
+            throw new Exception(static::EXCEPTION_MESSAGE_EMPTY_EVENT);
+        }
+
+        return $this->beforeEvent;
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function getAfterEvent(): EventInterface
+    {
+        if (! $this->afterEvent instanceof EventInterface) {
+            throw new Exception(static::EXCEPTION_MESSAGE_EMPTY_EVENT);
+        }
+
+        return $this->afterEvent;
     }
 }
